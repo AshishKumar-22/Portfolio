69cf3044e9a25f28b1a5c91685dd6b5465130b7a
Ashish Kumar <ashishkumar@Sumits-MacBook-Air.local>
2025-01-05 21:20:23 +0530

    added project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,7 +8,11 @@ import { PROJECTS } from '../constants';
 
 const Projects = () => {
 
+    const clickHandler = (e) => {
 
+        e.target.classList.toggle('opacity-0')
+        console.log(e.target.classList);
+    }
 
     return (
         <section className='pt-16' id='projects'>
@@ -18,13 +22,8 @@ const Projects = () => {
                     <motion.div initial={{ opacity: 0.9, }} whileInView={{ opacity: 1, scale: 1 }} transition={{ duration: 0.5 }} whileHover={{ scale: 1.05 }} key={project.id} className='border group relative overflow-hidden rounded-3xl lg:w-[390px] shrink h-60'>
                         <motion.img whileHover={{ scale: 1.1 }} src={project.image} alt={project.name} className='h-full w-full object-cover transition-transform duration-500 group-hover:scale-110 ' />
 
-                        <motion.div onTap={(e) => {
-                            const currentOpacity = window.getComputedStyle(e.target).opacity;
-
-                            e.target.style.opacity = currentOpacity == '1' ? '0' : '1'
-
-                        }} whileHover={{ opacity: 1 }} transition={{ duration: 0.5 }} className=' absolute inset-0 flex flex-col items-center justify-center text-white opacity-0  backdrop-blur-lg transition-opacity duration-500 group-hover:opacity-100'>
-                            <h2 className='mb-2 text-xl '>{project.name}</h2>
+                        <motion.div onClick={(e) => clickHandler(e)} whileHover={{ opacity: 1 }} transition={{ duration: 0.5 }} className={` absolute inset-0 flex flex-col items-center justify-center text-white opacity-0  backdrop-blur-lg transition-opacity duration-500 group-hover:opacity-100`}>
+                            <h2 className=' mb-2 text-xl '>{project.name}</h2>
                             <p className='mb-2 p-4 '>{project.description}</p>
 
                             <a href={project.githubLink} target='_blank' rel='noopener noreferrer' className='rounded-full bg-white px-4 py-2 text-black hover:bg-gray-300 '>
@@ -38,7 +37,7 @@ const Projects = () => {
                     </motion.div>
                 ))}
             </div>
-        </section>
+        </section >
     )
 }
 
