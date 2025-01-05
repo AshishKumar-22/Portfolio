0fbd6b1e49a9f343e4ff278ea01cb92000b9e2da
Ashish Kumar <ashishkumar@Sumits-MacBook-Air.local>
2025-01-05 19:53:54 +0530

    added new features

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { MdArrowOutward } from 'react-icons/md';
 import { motion } from 'framer-motion';
 import { FaCode } from 'react-icons/fa';
@@ -7,18 +7,38 @@ import { PROJECTS } from '../constants';
 
 
 const Projects = () => {
+
+    // const [isVisible, setIsVissible] = useState(false);
+    const handleClick = (e) => {
+
+        if (e.target.tagName === 'DIV') {
+            // Toggle opacity dynamically
+            const currentOpacity = getComputedStyle(e.target).opacity;
+            e.target.style.opacity = currentOpacity === '1' ? '0' : '1';
+
+            // Enable or disable pointer events for interaction
+            e.target.style.pointerEvents = currentOpacity === '1' ? 'none' : 'auto';
+        }
+
+        console.log(e.target.opacity);
+
+
+
+    }
+
     return (
         <section className='pt-16' id='projects'>
             <motion.h2 initial={{ opacity: 0, y: -20 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.8 }} className='mb-8 text-center text-3xl lg:text-4xl '>My Projects </motion.h2>
             <div className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3  '>
                 {PROJECTS.map((project, index) => (
                     <motion.div initial={{ opacity: 0.9, }} whileInView={{ opacity: 1, scale: 1 }} transition={{ duration: 0.5 }} whileHover={{ scale: 1.05 }} key={project.id} className='border group relative overflow-hidden rounded-3xl lg:w-[390px] shrink h-60'>
-                        <motion.img whileHover={{ scale: 1.1 }} onTap={{ scale: 1.1 }} src={project.image} alt={project.name} className='h-full w-full object-cover transition-transform duration-500 group-hover:scale-110 ' />
-                        <motion.div initial={{ opacity: 0 }} whileHover={{ opacity: 1 }} onTap={{ opacity: 1 }} transition={{ duration: 0.5 }} className='absolute inset-0 flex flex-col items-center justify-center text-white opacity-0 backdrop-blur-lg transition-opacity duration-500 group-hover:opacity-100'>
+                        <motion.img whileHover={{ scale: 1.1 }} src={project.image} alt={project.name} className='h-full w-full object-cover transition-transform duration-500 group-hover:scale-110 ' />
+
+                        <motion.div onTapStart={{ opacity: 1 }} onTapCancel={{ opacity: 0 }} whileHover={{ opacity: 1 }} onClick={(e) => handleClick(e)} transition={{ duration: 0.5 }} className=' absolute inset-0 flex flex-col items-center justify-center text-white opacity-0  backdrop-blur-lg transition-opacity duration-500 group-hover:opacity-100'>
                             <h2 className='mb-2 text-xl '>{project.name}</h2>
                             <p className='mb-2 p-4 '>{project.description}</p>
 
-                            <a href={project.githubLink} target='_blank' rel='noopener noreferrer' className=' rounded-full bg-white px-4 py-2 text-black hover:bg-gray-300  sm:text-base'>
+                            <a href={project.githubLink} target='_blank' rel='noopener noreferrer' className='rounded-full bg-white px-4 py-2 text-black hover:bg-gray-300 '>
                                 <div className='flex items-center'>
                                     <span>View </span><span>{'\u00A0'}</span>
                                     <FaEye />
